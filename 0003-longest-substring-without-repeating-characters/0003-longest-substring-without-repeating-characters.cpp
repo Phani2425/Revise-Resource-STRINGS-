1e2d44a50de9e607016f78aedcd3bbe4244e2db3
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-02-20 01:35:35 +0530

    Time: 9 ms (78.44%), Space: 10.4 MB (57.98%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int longest=0;//this  variable will store the length of longest possible substring which wii not have any  repeatinng character
+        int first=0,second=0;//two pointer where second pointer will check wheather any character has already occured before
+        //betwwen current first and second pointer and according to this firrst pointeer will move to remove that already occured character from the current  substring
+
+        vector<int> map(256,0);//this is a vector  which will be used as map to keep track of  wheather any character has already occured between current first and second pointer
+        //the character that already occured  will have 1 in the corresponding index off the map otherwise it will contain 0 in correspondingg index of character
+        //when ever we will found any  unooccuered chaaracter in second pointer theen we will put 1 inn the corresponding index of that character
+        //and when we will found any already occured characcter in second pointer then we will move first pointer and put 0 for all characters in their correspondding  index of map untill we reach that character
+        //when first pointer will reach that character we will put 0 in the character's corresponding index of map and move first pointer to its next index
+        //and we will do all these untill seconnd pointeer is less  than the size of string or until the second pointer crosses the last index of string s
+
+        while(second<s.size()){
+            if(map[s[second]]==0){//if character is unoccured
+                map[s[second]]=1;
+                longest=max(longest,(second-first+1));//maximum value between these two will be stored in longeest variable
+                //it is liitle bit simillar to greedy approach
+                //as we are storing ans and greedily trying to find more lengthy substring having no repeating character
+                second++;
+            }
+
+            else{//if element pointed by secondd pointer has already occured ( in the substring betwwen current first and second pointer){IMP}
+                while(s[first]!=s[second]){
+                    map[s[first]]=0;//declare all character pointed by first unoccureed by puuting 0 in their corresponding index of map
+                    first++;//move first
+                }
+                //now at this point  first would be pointing the charaacter we want to remove from our substring now
+                //so lets reemove that by doing first++.....
+                //but before that lets declare that unoccured as we are removing that from our current substring hence for our cuurent substring it is not occuered yet
+                map[s[first]]=0;
+                    first++;
+            }
+        }
+        //now reeturn result
+        return longest;
+    }
+};
