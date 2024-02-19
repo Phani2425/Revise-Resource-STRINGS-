2e83f0c10a56f4c12304b02003614dc37887bcd3
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-02-19 18:59:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -7,17 +7,22 @@ public:
         }
 
         int count=1;
-        string ans;
-        int i;
-        for( i=0;i<chars.size()-1;i++){
+        string str;//it ewill be used to strore when count will be converted into string
+        int index=0;//this will indicate the index of chars array as using this we will direct put the character in to char array
+        for(int i=0;i<chars.size()-1;i++){
             if(chars[i]==chars[i+1]){
                 count++;
             }
             else{
-                ans.push_back(chars[i]);
+                chars[index]=chars[i];
+                index++;
                 if (count > 1) {
                 // Convert the count to a string and append it digit by digit
-                ans=ans+to_string(count);
+                str=to_string(count);
+                for(char ch:str){
+                    chars[index]=ch;
+                    index++;
+                }
                 }
                 count=1;
             }
@@ -25,19 +30,31 @@ public:
           int n=chars.size()-1;
         if(chars[n]==chars[n-1]){
                 
-                    ans.push_back(chars[n]);
-                    ans=ans+to_string(count);
+                    chars[index]=chars[n];
+                    index++;
+                    str=to_string(count);
+                    for(char ch:str){
+                    chars[index]=ch;
+                    index++;
+                    }
+                     
                }
         else{
-                    ans.push_back(chars[n]);
+                    chars[index]=chars[n];
+                    index++;
                 }  
                 
 
-        int index=0;
-        for(char ch:ans){
-           chars[index]=ch;
-           index++;
-        }
-        return ans.size();
+        return index;//this is the most  important point 
+        //here we can notice that we are not completely editing the chars array we are just edititng the chars array upto a certain index
+        //for example we are edititng the chars array of given 1st example only upto its 5th index
+        //other indexes will be same as they are
+        //whatever value we will return here compiler will check only that size of char array
+        //if we return 5 then it will check from 0 to 4th index of array i.e subaary of 5 size
+        //if we return 6 then it will check  6 sized subarray starting from 0 index to 5th index
+        //so here we will return the size of our edited array
+        //and that size will always be 1 larger than the index upto which we edited as this is a 0 indexed array
+        //hence as we are editing the array using "index" and every time we edit something index++ happens
+        //hence at last it can give us our size of edited array
     }
 };
