1f4e41e428a24b3b323539a63f80437fc8ddd051
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-02-20 22:06:56 +0530

    Time: 12 ms (74.59%), Space: 20.4 MB (18.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -11,6 +11,9 @@ public:
         // }
 
         // return sum-sum2;
+
+        //......................................USING XOR.............................................
+
         int result=0;
         for(int i=0;i<=nums.size();i++){
           result=result^i;
@@ -22,5 +25,31 @@ public:
 
         return result;
 
+        //....................................USING BINARY SEARCH OR DIVIDE AND CONQUER CONCEPPT..............................
+        
+        // sort(nums.begin(),nums.end());//  sorting the array to apply binary search
+        // if(nums[0]==1){
+        //     return 0;//if 0 is the missing element then directly return 0 as our program cannot handle that case
+        //     //why it cant handle the case when 0 is missing elemennt ????? Dry run that case with program to know abouuut it
+        // }
+        // int start=0,end=nums.size()-1;
+        // int ansindex;
+
+        // while(start<=end){
+        //     int mid=start+(end-start)/2;
+
+        //     if(mid==nums[mid]){//after sorting if the value stored in  a index is same as that index that will mean that untill that element no element is missing and the missing element may lie in right side of that index
+        //     //also we know that if the value stored in  a index is same as that index then that value could be the previous value of missing element
+        //     //hence we will store that and then move  to right by doing start=mid+1 
+
+        //         ansindex=mid;
+        //         start=mid+1;
+        //     }
+        //     else if(mid<nums[mid]){
+        //         end=mid-1;
+        //     }
+
+        // }
+        // return ansindex+1;
     }
 };
