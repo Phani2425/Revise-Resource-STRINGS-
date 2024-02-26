17dd5ce6a5bdd76d244d24b0425911d463fd9d7e
Phani Bhusan Mohanty <129371230+Phani2425@users.noreply.github.com>
2024-02-27 02:49:57 +0530

    Time: 51 ms (81.96%), Space: 21.1 MB (73.12%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-
-    bool is_palindrome(string s,int start,int end){
+//function for checking palindrome or not
+    bool Palindrom(string &s,int start,int end){
         while(start<=end){
             if(s[start]==s[end]){
                 start++;
@@ -15,29 +15,29 @@ public:
     }
 
     bool validPalindrome(string s) {
-        int start=0,end=s.length()-1;
-        while(start<=end){
-            if(s[start]==s[end]){
-                start++;
-                end--;
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
             }
             else{
-                //ek baar start pointed character ko hatake dekhenge
-                if(is_palindrome(s,start+1,end)==true){
-                    return true;
-                }
+                //s[i]!=s[j]
+                //1 removal is allowed
+                //we will remove value pointed by i and j one by one and recheck unchecked substring
 
-                //phir end pointed character to hatakee dekhenge
-                else if(is_palindrome(s,start,end-1)==true){
-                    return true;
-                }
+                //checking remaining unchecked array after removing value pointed by i
+                bool ans1=Palindrom(s,i+1,j);
 
-                else{
-                    return false;
-                }
+                 //checking remaining unchecked array after removing value pointed by j
+                 bool ans2=Palindrom(s,i,j-1);
+
+                 //returning true if any one of both is palindrome
+                 return ans1||ans2;
             }
         }
-
+        //agar yaha tak pahunche ho then 0 removal and string originally palindrome hai
         return true;
+        
     }
 };
